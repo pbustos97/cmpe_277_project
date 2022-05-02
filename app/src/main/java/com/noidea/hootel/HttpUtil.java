64379ce0f3dbee19ce6f312e6a11bdfa88298bd4
Handsomenick1 <58188159+Handsomenick1@users.noreply.github.com>
2022-05-02 11:12:01 -0500

    add GET/POST/PATCH requsts

---
+++ app/src/main/java/com/noidea/hootel/HttpUtil.java
@@ -0,0 +1,98 @@
+package com.noidea.hootel;
+import android.text.TextUtils;
+import android.util.Log;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import java.net.URL;
+
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class HttpUtil extends Thread {
+    private static final String ROOM_URL = "https://nua5fhfin1.execute-api.us-west-2.amazonaws.com/prod/";
+
+    public static JSONObject getJSON(String endpoint) throws JSONException {
+        String jsonString = "";
+        try {
+            URL url = new URL(ROOM_URL + endpoint);
+
+            Log.d("RestHelper", "getJSON url: " + url.toString());
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setConnectTimeout(10000);
+            connection.setReadTimeout(10000);
+            connection.setDoOutput(true);
+            Log.d("RestHelper", "connection created");
+            connection.setRequestMethod("GET");
+            Log.d("RestHelper", "set request method");
+            connection.connect();
+            Log.d("RestHelper", "getJSON Connected");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                Log.d("RestHelper", "getJSON read: " + line);
+            }
+            br.close();
+
+            jsonString = sb.toString();
+            Log.d("RestHelper", "getJSON jsonString: " + jsonString);
+
+        } catch (Exception e) {
+            Log.e("RestHelper", "getJSON error: " + e.getMessage());
+        }
+        return new JSONObject(jsonString);
+    }
+
+    public static JSONObject sendRequest(String endpoint, String Method, String Json) throws JSONException {
+        BufferedReader reader = null;
+        String result = "";
+
+        try {
+            URL url = new URL(ROOM_URL + endpoint);
+
+            Log.d("RestHelper", "getJSON url: " + url.toString());
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setConnectTimeout(10000);
+            connection.setReadTimeout(10000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setRequestMethod(Method);
+            connection.setUseCaches(false);
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("Charset", "UTF-8");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("accept", "*/*");
+            StringBuilder sb = new StringBuilder();
+            if (Json != null && !TextUtils.isEmpty(Json)) {
+                connection.setRequestProperty("Content-Length", String.valueOf(Json.length()));
+                OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+                out.append(Json);
+                out.flush();
+                out.close();
+                Log.d("RestHelper", String.valueOf(connection.getResponseCode()));
+
+            }
+            if (connection.getResponseCode() == 200) {
+                reader = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+                result = reader.readLine();
+                Log.d("RestHelper", "result: " + result);
+
+            }
+        } catch (IOException e) {
+            Log.e("RestHelper", "getJSON error: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return new JSONObject(result);
+    }
+
+
+}
--- app/src/main/java/com/noidea/hootel/MainActivity.java
+++ app/src/main/java/com/noidea/hootel/MainActivity.java
@@ -4,11 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.json.JSONException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    HttpUtil.getJSON("allroomInfo");
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpUtil.sendRequest("amenity", "PATCH", "{\n" +
+                            "    \"body\" : {\n" +
+                            "        \"amenityId\" : \"6c9c1801-0acd-41ee-8274-f1a41c678a39\", \n" +
+                            "            \"amenityInfo\" :\n" +
+                            "            {\"amenityName\" : \"test change\"\n" +
+                            "            }\n" +
+                            "        \n" +
+                            "    }\n" +
+                            "}");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
